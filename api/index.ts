c52eff0c88c16032e3d362e21fa943fa8941b6cf
Anthony Leon <anthonyfabian22lldev@gmail.com>
2024-11-25 21:53:46 +0100

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import {Hono} from 'hono';
+import {handle} from 'hono/vercel';
+
+const app = new Hono().basePath('/api');
+
+app.get('/', (c) => {
+    return c.json({message: "Congrats! You've deployed Hono to Vercel"});
+});
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
