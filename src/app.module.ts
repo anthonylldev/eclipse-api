396fd54a0c8c867caa696f77a2d13b77627a5886
Anthony Leon <anthonyfabian22lldev@gmail.com>
2025-02-07 19:01:51 +0100

    feat: config mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI
+        ? process.env.MONGO_URI
+        : throwError('MONGO_URI is not defined'),
+    ),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
+function throwError(message: string): never {
+  throw new Error(message);
+}
